55f5d3abe839446e5813138e24a2c8edb30c609f
Todd Wolfson <todd@twolfson.com>
2015-05-29 22:24:53 -0500

    Made sure all logic worked out

--- httpretty_fixtures/__init__.py
+++ httpretty_fixtures/__init__.py
@@ -13,8 +13,7 @@ class FixtureManager(object):
     # https://docs.python.org/2/tutorial/classes.html#private-variables-and-class-local-references
     __fixtures = {}
 
-    @classmethod
-    def run(cls, fixtures):
+    def run(self, fixtures):
         """
         Decorator to start up `httpretty` with a set of fixtures
 
@@ -25,7 +24,7 @@ class FixtureManager(object):
         # def test_request_hello(self, arg1, arg2):
         #   pass
 
-        # `cls` is `FakeElasticsearch`
+        # `self` is `FakeElasticsearch`
         # `fixtures` is `['hello']`
         # We are initially executing `FakeElasticsearch.run(['hello'])` as if were a function
 
@@ -43,13 +42,13 @@ class FixtureManager(object):
             @functools.wraps(fn)
             def wrapper(self, *args, **kwargs):
                 # Start our class
-                cls.start()
+                self.start()
 
                 # Run our fn and always cleanup
                 try:
                     return fn(self, *args, **kwargs)
                 finally:
-                    cls.stop()
+                    self.stop()
 
             # Return our wrapped function
             #   i.e. `test_request_hello` with some before/after logic (of start/stop)
@@ -59,8 +58,7 @@ class FixtureManager(object):
         #  i.e. `decorator_fn` to process `test_request_hello`
         return decorate_fn
 
-    @classmethod
-    def save_fixture(cls, name, *register_uri_args, **register_uri_kwargs):
+    def save_fixture(self, name, *register_uri_args, **register_uri_kwargs):
         """
         Save a fixture to our class for usage later on
 
@@ -69,62 +67,69 @@ class FixtureManager(object):
         :param **kwargs register_uri_kwargs: Keyword arguments to pass through to `httpretty.register_uri`
         """
         # If there already is a fixture, complain
-        if name in cls.__fixtures:
+        if name in self.__fixtures:
             raise RuntimeError('Key "{name}" already exists in fixtures for `httpretty-fixtures`'.format(name=name))
 
         # Otherwise, save our fixture
-        cls.__fixtures[name] = {
+        self.__fixtures[name] = {
             'args': register_uri_args,
             'kwargs': register_uri_kwargs,
         }
 
     # https://github.com/gabrielfalcao/HTTPretty/blob/0.8.3/httpretty/core.py#L1023-L1032
     # https://github.com/spulec/moto/blob/0.4.2/moto/core/models.py#L32-L65
-    def start(cls):
+    def start(self):
         """Start running this class' fixtures"""
         # Increase our internal counter
-        cls.nested_count += 1
+        self.nested_count += 1
 
         # If HTTPretty hasn't been started yet, then reset its info start it
         if not HTTPretty.is_enabled():
             HTTPretty.enable()
 
         # For each of our fixtures, bind them
-        for name in cls.__fixtures:
-            fixture = cls.__fixtures[name]
+        for name in self.__fixtures:
+            fixture = self.__fixtures[name]
             HTTPretty.register_uri(*fixture['args'], **fixture['kwargs'])
 
-    def stop(cls):
+    def stop(self):
         """Stop running this class' fixtures"""
         # Decrease our counter
-        cls.nested_count -= 1
+        self.nested_count -= 1
 
         # If we have stopped running too many times, complain and leave
-        if cls.nested_count < 0:
+        if self.nested_count < 0:
             raise RuntimeError('When running `httpretty-fixtures`, `stop()`'
                                'was run more times than (or before) `start()`')
 
         # If we have gotten out of nesting, then stop HTTPretty and
-        if cls.nested_count == 0:
+        if self.nested_count == 0:
             HTTPretty.disable()
-
 
 # Define our helper registration methods
 # https://github.com/gabrielfalcao/HTTPretty/blob/0.8.3/httpretty/http.py#L112-L121
-def get_fixture_method(httpretty_method):
-    """Helper to easily generate per-method fixture bindings"""
-    def save_fixture_by_method(fixture_fn, *register_uri_args, **register_uri_kwargs):
-        # Register our URL under the fixture's name
-        body = fixture_fn
-        cls.save_fixture(fixture_fn.__name__, httpretty_method, *register_uri_args, body=body, **register_uri_kwargs)
+_method_map = {
+    'GET': 'get',
+    'PUT': 'put',
+    'POST': 'post',
+    'DELETE': 'delete',
+    'HEAD': 'head',
+    'PATCH': 'patch',
+    'OPTIONS': 'options',
+    'CONNECT': 'connect',
+}
+for httpretty_method in HTTPretty.METHODS:
+    def save_fixture_by_method(self, *register_uri_args, **register_uri_kwargs):
+        def save_fixture_by_method_decorator(fixture_fn):
+            # Register our URL under the fixture's name
+            body = fixture_fn
+            self.save_fixture(fixture_fn.__name__, httpretty_method,
+                              *register_uri_args, body=body, **register_uri_kwargs)
 
-        # Return our function for reuse
-        return fixture_fn
-get = get_fixture_method(HTTPretty.GET)
-put = get_fixture_method(HTTPretty.PUT)
-post = get_fixture_method(HTTPretty.POST)
-delete = get_fixture_method(HTTPretty.DELETE)
-head = get_fixture_method(HTTPretty.HEAD)
-patch = get_fixture_method(HTTPretty.PATCH)
-options = get_fixture_method(HTTPretty.OPTIONS)
-connect = get_fixture_method(HTTPretty.CONNECT)
+            # Return our function for reuse
+            return fixture_fn
+
+        # Return our decorator
+        return save_fixture_by_method_decorator
+    class_key = _method_map[httpretty_method]
+    setattr(FixtureManager, class_key, save_fixture_by_method)
--- httpretty_fixtures/test/test.py
+++ httpretty_fixtures/test/test.py
@@ -4,13 +4,16 @@ import httpretty_fixtures
 
 
 # Set up multiple fixture managers
-class FakeServer(httpretty_fixtures.FixtureManager):
-    @httpretty_fixtures.get('http://localhost:9000/')
-    def hello(self, request, uri, res_headers):
-        return (200, res_headers, 'world')
+FakeServer = httpretty_fixtures.FixtureManager()
+
+
+@FakeServer.get('http://localhost:9000/')
+def hello(self, request, uri, res_headers):
+    return (200, res_headers, 'world')
 
 
 class TestHttprettyFixtures(TestCase):
+    @FakeServer.run(['hello'])
     def test_request(self):
         """
         A request to a non-existant server behind a running FixtureManager
