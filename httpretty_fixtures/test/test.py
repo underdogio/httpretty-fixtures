d2448450361388d475965b9cb61564556aded1e1
Todd Wolfson <todd@twolfson.com>
2015-06-02 12:50:11 -0500

    Added more tests

--- httpretty_fixtures/test/test.py
+++ httpretty_fixtures/test/test.py
@@ -13,6 +13,18 @@ class FakeServer(httpretty_fixtures.FixtureManager):
         return (200, res_headers, 'world')
 
 
+class CounterServer(httpretty_fixtures.FixtureManager):
+    def __init__(self):
+        self.count = 0
+        super(CounterServer, self).__init__()
+
+    @httpretty_fixtures.get('http://localhost:9000/')
+    def counter(self, request, uri, res_headers):
+        self.count += 1
+        return (200, res_headers, str(self.count))
+
+
+# Define our tests
 class TestHttprettyFixtures(TestCase):
     @FakeServer.run(['hello'])
     def test_request(self):
@@ -51,3 +63,30 @@ class TestHttprettyFixtures(TestCase):
         self.assertEqual(len(httpretty_fixtures.requests()), 2)
         self.assertEqual(httpretty_fixtures.requests()[0].path, '/?first')
         self.assertEqual(httpretty_fixtures.requests()[1].path, '/?second')
+
+    @CounterServer.run(['count'])
+    def test_state_preserved(self):
+        """
+        Multiple stateful requests to a running FixtureManager
+            receive appropriate state
+        """
+        # Make our first request and verify its count
+        res = requests.get('http://localhost:9000/')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.text, '1')
+
+        # Make our second request and verify its count
+        res = requests.get('http://localhost:9000/')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.text, '2')
+
+    @CounterServer.run(['count'])
+    def test_state_disjoint(self):
+        """
+        A separately running FixtureManager
+            does not receive state from past runs
+        """
+        # Verifies that we don't get bleed from `test_state_preserved`
+        res = requests.get('http://localhost:9000/')
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res.text, '1')
